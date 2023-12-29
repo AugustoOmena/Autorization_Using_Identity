e39a541202eaeb50d965dc7dd3591fa83efc5780
Augusto Omena <augusto.omena.n@gmail.com>
2023-12-29 14:29:28 -0300

    Add project files.

---
+++ Migrations/20231229171332_SecondMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebAppTestePS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "08549592-c589-437e-a310-963d91fef219", null, "admin", "admin" },
+                    { "56ca6221-ab61-4133-b223-27d2e0285277", null, "client", "seller" },
+                    { "bf1aa84d-7199-4383-b62a-b85d96aa8a96", null, "seller", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "08549592-c589-437e-a310-963d91fef219");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "56ca6221-ab61-4133-b223-27d2e0285277");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bf1aa84d-7199-4383-b62a-b85d96aa8a96");
+        }
+    }
+}
