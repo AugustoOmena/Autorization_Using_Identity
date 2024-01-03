ebdf3cb2104ce9e950196079cff1a29f5a023be6
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2024-01-03 13:16:29 -0300

    Adicionando PessoaPropaganda, slide de imagens e chamamento para criar conta.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/* imagem PessoaPropaganda */
+var images = ["1.png", "2.png", "3.png", "4.png"];
+var index = 0;
+
+function changeImage() {
+    var imagePath = "img/PessoaPropaganda/" + images[index];
+    document.getElementById("changingImage").src = imagePath;
+    index = (index + 1) % images.length;
+}
+
+// Change image every 2 seconds (2000 milliseconds)
+setInterval(changeImage, 2000);
