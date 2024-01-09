e3e2f32e0cc1b9cbd69257c7eb4468c41516a2a3
Augusto Omena <augusto.omena.n@gmail.com>
2024-01-09 13:33:16 -0300

    Conexão com banco local, barra de pesquisa e funcionalidades.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -26,3 +26,53 @@ document.getElementById('openModalPesquisaBtn').addEventListener('click', functi
     var myModal = new bootstrap.Modal(document.getElementById('myModalPesquisa'));
     myModal.show();
 });
+
+/* Cidade Escolhida */
+$(document).ready(function () {
+    // Recupera a escolha da cidade do armazenamento local
+    var cidadeEscolhida = localStorage.getItem('cidadeEscolhida');
+
+    // Verifica se cidadeEscolhida é nula ou indefinida
+    if (cidadeEscolhida === null || cidadeEscolhida === undefined) {
+        cidadeEscolhida = "Cidades";
+    }
+
+    // Define a cidade escolhida no elemento de exibição
+    $("#cidadeEscolhida").text(cidadeEscolhida);
+
+    // Configura o evento de clique no botão
+    $("#enviarBtn").click(function () {
+        // Obtém a cidade escolhida
+        var cidadeEscolhida = $("#cidadeSelect").val();
+
+        // Armazena a escolha da cidade no armazenamento local
+        localStorage.setItem('cidadeEscolhida', cidadeEscolhida);
+
+        // Atualiza o elemento de exibição
+        $("#cidadeEscolhida").text(cidadeEscolhida);
+    });
+
+    /* barra pesquisa */
+    $(document).ready(function () {
+        $("#searchIcon").click(function () {
+            // Verifica se há texto no input e uma cidade escolhida
+            var searchText = $("#searchInput").val();
+            var cidadeEscolhida = $("#cidadeEscolhida").text();
+
+            if (searchText && cidadeEscolhida !== "Cidade") {
+                // Realiza a pesquisa (coloque aqui o código que você deseja executar ao pesquisar)
+                console.log("Pesquisar: " + searchText + " na cidade: " + cidadeEscolhida);
+            } else {
+                alert("Por favor, insira texto e escolha uma cidade para pesquisar.");
+            }
+        });
+
+        $("#closeIcon").click(function () {
+            // Limpa o input e esconde o ícone de fechar
+            $("#searchInput").val("");
+            $("#closeIcon").hide();
+        });
+
+        // Outras funcionalidades podem ser adicionadas conforme necessário
+    });
+});
