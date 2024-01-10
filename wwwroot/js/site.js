8b4b10018d04fc3de9a02a115522d3eee4671bfc
Augusto Omena <augusto.omena.n@gmail.com>
2024-01-10 13:07:10 -0300

    Backand da Localização.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,78 +1,63 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-/* imagem PessoaPropaganda */
-var images = ["1.png", "2.png", "3.png", "4.png"];
+﻿/* imagem PessoaPropaganda */
+var imagesPessoaPropaganda = ["1.png", "2.png", "3.png", "4.png"];
 var index = 0;
+var cidadesValidas = [
+    "rio", "niteroi", "sao-goncalo", "paraty", "trindade", "ilha-grande",
+    "angra", "ilha-madeira", "ilha-itacurucá", "ilha-martins", "itimirim",
+    "itingucu", "mangaratiba", "rio-claro", "conceicao-jacarei",
+    "sao-jose-barreiro", "ubatuba", "caraguatatuba", "sao-sebastiao", "ilhabela"
+];
 
 function changeImage() {
-    var imagePath = "img/PessoaPropaganda/" + images[index];
+    var imagePath = "img/PessoaPropaganda/" + imagesPessoaPropaganda[index];
     document.getElementById("changingImage").src = imagePath;
-    index = (index + 1) % images.length;
+    index = (index + 1) % imagesPessoaPropaganda.length;
 }
-
-// Change image every 2 seconds (2000 milliseconds)
 setInterval(changeImage, 2000);
 
 /*Script para manipular os modais*/
-document.getElementById('openModalBtn').addEventListener('click', function () {
-    var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-    myModal.show();
+document.getElementById('ModalCadastroDeClientePremium').addEventListener('click', function () {
+    var ModalPremiums = new bootstrap.Modal(document.getElementById('McriarPremium'));
+    ModalPremiums.show();
 });
 
-document.getElementById('openModalPesquisaBtn').addEventListener('click', function () {
-    var myModal = new bootstrap.Modal(document.getElementById('myModalPesquisa'));
-    myModal.show();
+document.getElementById('ChamaModalSelecionarCidade').addEventListener('click', function () {
+    var ModalCidade = new bootstrap.Modal(document.getElementById('MselecionarCidade'));
+    ModalCidade.show();
 });
 
-/* Cidade Escolhida */
+function SeForNuloRecebeCidade(cidadeEscolhida) {
+    if (cidadeEscolhida === null || cidadeEscolhida === undefined) {
+        cidadeEscolhida = "Cidade";
+    }
+    return cidadeEscolhida;
+}
+
+function SeForNuloRecebeFrase(cidadeEscolhida) {
+    if (cidadeEscolhida === null || cidadeEscolhida === undefined) {
+        cidadeEscolhida = "Cidade";
+    }
+    return cidadeEscolhida;
+}
+
+
 $(document).ready(function () {
     // Recupera a escolha da cidade do armazenamento local
-    var cidadeEscolhida = localStorage.getItem('cidadeEscolhida');
+    var cidadeEscolhida = SeForNuloRecebeCidade(localStorage.getItem('cidadeEscolhida'));
+    var temCidadeOuNao = cidadesValidas.includes(cidadeEscolhida);
 
-    // Verifica se cidadeEscolhida é nula ou indefinida
-    if (cidadeEscolhida === null || cidadeEscolhida === undefined) {
-        cidadeEscolhida = "Cidades";
-    }
-
-    // Define a cidade escolhida no elemento de exibição
     $("#cidadeEscolhida").text(cidadeEscolhida);
+    $("#TextoBotaoCidade").text(cidadeEscolhida);
+    $(".comCidade").toggleClass("d-none", !temCidadeOuNao);
+    $(".semCidade").toggleClass("d-none", temCidadeOuNao);
+});
 
-    // Configura o evento de clique no botão
-    $("#enviarBtn").click(function () {
-        // Obtém a cidade escolhida
-        var cidadeEscolhida = $("#cidadeSelect").val();
 
-        // Armazena a escolha da cidade no armazenamento local
-        localStorage.setItem('cidadeEscolhida', cidadeEscolhida);
-
-        // Atualiza o elemento de exibição
-        $("#cidadeEscolhida").text(cidadeEscolhida);
-    });
-
-    /* barra pesquisa */
-    $(document).ready(function () {
-        $("#searchIcon").click(function () {
-            // Verifica se há texto no input e uma cidade escolhida
-            var searchText = $("#searchInput").val();
-            var cidadeEscolhida = $("#cidadeEscolhida").text();
-
-            if (searchText && cidadeEscolhida !== "Cidade") {
-                // Realiza a pesquisa (coloque aqui o código que você deseja executar ao pesquisar)
-                console.log("Pesquisar: " + searchText + " na cidade: " + cidadeEscolhida);
-            } else {
-                alert("Por favor, insira texto e escolha uma cidade para pesquisar.");
-            }
-        });
-
-        $("#closeIcon").click(function () {
-            // Limpa o input e esconde o ícone de fechar
-            $("#searchInput").val("");
-            $("#closeIcon").hide();
-        });
-
-        // Outras funcionalidades podem ser adicionadas conforme necessário
-    });
+// Configura o evento de clique no botão
+$("#selecionarCidade").click(function () {
+    var cidadeEscolhida = SeForNuloRecebeCidade($("#cidadeSelect").val());
+    localStorage.setItem('cidadeEscolhida', cidadeEscolhida);//lembrar da cidadeEscolhida
+    $("#cidadeEscolhida").text(cidadeEscolhida);
+    $("#TextoBotaoCidade").text(cidadeEscolhida);
+    $("#MselecionarCidade").modal("hide");
 });
